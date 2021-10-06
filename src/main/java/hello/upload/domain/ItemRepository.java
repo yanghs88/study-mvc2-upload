99eef196a1e50c32f20ba6e887f31aec567eaa80
양희서/전시개발팀 <victor.yang@musinsa.com>
2021-10-06 21:25:04 +0900

    섹션 11. 파일업로드 수강 완료

---
+++ src/main/java/hello/upload/domain/ItemRepository.java
@@ -0,0 +1,24 @@
+package hello.upload.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemRepository {
+
+    private final Map<Long, Item> store = new HashMap<>();
+    private long sequence = 0L;
+
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+}
